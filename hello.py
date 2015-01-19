74a9f49d3dcf178353fb4d54bcdcceedc470c43e
zhangshy <zaozh1990@163.com>
2015-01-20 02:35:31 +0800

    添加消息闪现

--- hello.py
+++ hello.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, \
+    request, url_for
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+            request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 @app.route('/hello')
-def hello():
-    return 'Hello World'
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
 
 
 if __name__ == '__main__':
