7bfb548fa10c0716046aa1c9afc608bef1c3dcd2
zhangshy <zaozh1990@163.com>
2015-01-23 02:57:05 +0800

    整理目录并添加login

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from flask import Flask, flash, redirect, render_template, \
-    request, url_for
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from flask.ext.wtf import Form
 from wtforms.fields import StringField, BooleanField
 from wtforms.validators import DataRequired
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from blog import app
+from blog import app, db, lm, oid
+from models import User
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
@@ -16,16 +17,44 @@ class LoginForm(Form):
 def index():
     return render_template('index.html')
 
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 @app.route('/idlogin', methods=['GET', 'POST'])
+@oid.loginhandler()
 def idlogin():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect(url_for('index'))
+        session['remember_me'] = form.remember_me.data
+#        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('idlogin.html',
                     title = 'Sign In',
                     form = form,
                     providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('idlogin'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname=="":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
